89cc1fcafb6a3967dfda2425fb4c44f5e7346db2
JD3M0N <josuedeki21@gmail.com>
2025-06-07 15:56:04 -0400

    updated api

--- src/NLP/Gemini.py
+++ src/NLP/Gemini.py
@@ -1,24 +1,21 @@
 from dotenv import load_dotenv
 import os
+import google.generativeai as genai
 
 load_dotenv()
 api_key = os.getenv("API_KEY_GEMINI")
-from google import genai
 
 class Gemini():
-    def __init__(self, model : str = "gemini-2.0-flash"):
+    def __init__(self, model: str = "gemini-2.0-flash"):
         self.model = model
-        self.client = genai.Client(api_key=api_key)
-        
+        genai.configure(api_key=api_key)
 
     def ask(self, prompt: str):
-        response = self.client.models.generate_content(
-                model= self.model, contents=prompt
-            )
+        model = genai.GenerativeModel(self.model)
+        response = model.generate_content(prompt)
         return response.text
-
 
 if __name__ == "__main__":
     gemini = Gemini()
-    response = gemini.ask("What is the capital of France?")
-    print(response)  # Should print "Paris"
+    response = gemini.ask("Who is Abel Ponce Gonzales?")
+    print(response)
